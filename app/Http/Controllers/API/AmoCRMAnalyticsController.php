f79fd46a04941f2da8127706a48b2d6c9acd4c17
Aleksandr Sazhin <i4j5@ya.ru>
2021-06-10 11:14:12 +0300

    api analytics

--- app/Http/Controllers/API/AmoCRMAnalyticsController.php
+++ app/Http/Controllers/API/AmoCRMAnalyticsController.php
@@ -16,6 +16,9 @@ class AmoCRMAnalyticsController extends Controller
         $this->api = new RequestActions;
     }
 
+    /**
+     * Новые лиды
+     */
     public function NewLeads(Request $request) {
 
         $created_from = $request->input('created_from');
@@ -58,11 +61,39 @@ class AmoCRMAnalyticsController extends Controller
 
         }
 
+        $all_price = 0;
+        $cost_price = 0;
+        $paid = 0;
+        foreach ($leads as $lead) {
+            $all_price = $all_price + $lead->price;
+
+            if (isset($lead->custom_fields_values)) {
+                foreach ($lead->custom_fields_values as $field) {
+                    
+                    // Себестоимость
+                    if ($field->field_id == 75197) {
+                        $cost_price = $cost_price + $field->values[0]->value;
+                    }
+
+                    // Оплачено клиентом
+                    if ($field->field_id == 506505) {
+                        $paid = $paid + $field->values[0]->value;
+                    }
+                }
+            }
+        }
+
         return [
+            'all_price' => $all_price,
+            'cost_price' => $cost_price,
+            'paid' => $paid,
             'count' => count($leads)
         ];
     }
 
+    /**
+     * Переданы в производство
+     */
     public function ProductionLeads(Request $request) {
 
         $created_from = $request->input('created_from');
@@ -121,11 +152,39 @@ class AmoCRMAnalyticsController extends Controller
 
         }
 
+        $all_price = 0;
+        $cost_price = 0;
+        $paid = 0;
+        foreach ($leads as $lead) {
+            $all_price = $all_price + $lead->price;
+
+            if (isset($lead->custom_fields_values)) {
+                foreach ($lead->custom_fields_values as $field) {
+                    
+                    // Себестоимость
+                    if ($field->field_id == 75197) {
+                        $cost_price = $cost_price + $field->values[0]->value;
+                    }
+
+                    // Оплачено клиентом
+                    if ($field->field_id == 506505) {
+                        $paid = $paid + $field->values[0]->value;
+                    }
+                }
+            }
+        }
+
         return [
+            'all_price' => $all_price,
+            'cost_price' => $cost_price,
+            'paid' => $paid,
             'count' => count($leads)
         ];
     }
 
+    /**
+     * Активные сделки
+     */
     public function ActiveLeads(Request $request) {
 
         $created_from = $request->input('created_from');
@@ -250,11 +309,39 @@ class AmoCRMAnalyticsController extends Controller
 
         }
 
+        $all_price = 0;
+        $cost_price = 0;
+        $paid = 0;
+        foreach ($leads as $lead) {
+            $all_price = $all_price + $lead->price;
+
+            if (isset($lead->custom_fields_values)) {
+                foreach ($lead->custom_fields_values as $field) {
+                    
+                    // Себестоимость
+                    if ($field->field_id == 75197) {
+                        $cost_price = $cost_price + $field->values[0]->value;
+                    }
+
+                    // Оплачено клиентом
+                    if ($field->field_id == 506505) {
+                        $paid = $paid + $field->values[0]->value;
+                    }
+                }
+            }
+        }
+
         return [
+            'all_price' => $all_price,
+            'cost_price' => $cost_price,
+            'paid' => $paid,
             'count' => count($leads)
         ];
     }
 
+    /**
+     * Закрытые сделки
+     */
     public function ClosedLeads(Request $request) {
 
 
@@ -263,6 +350,106 @@ class AmoCRMAnalyticsController extends Controller
 
         $closed_from = $request->input('closed_from');
         $closed_to = $request->input('closed_to');
+        
+
+        $created_at = [
+            'from' => strtotime($created_from),
+            'to' => strtotime("$created_to 23:59:59.000"),
+        ];
+
+        $closed_at = [
+            'from' => strtotime($closed_from),
+            'to' => strtotime("$closed_to 23:59:59.000"),
+        ];
+
+        $leads = [];
+        $limit = 200;
+
+        for ($page=1, $run=true; $run; $page++) { 
+            
+            $data = [
+                'filter' => [
+                    
+                    'statuses' => [
+                        0 => [
+                            'pipeline_id' => 2291194,
+                            'status_id' => 142
+                        ]
+                    ],
+                    'created_at' => $created_at,
+                    'closed_at' => $closed_at,
+                    'page' => $page,
+                ],
+                'limit' => $limit 
+            ];
+    
+            $query = http_build_query($data);
+    
+            $res = $this->api->execute("/api/v4/leads?$query");
+
+            if ($res) {
+
+                $arr = $res->_embedded->leads;
+
+                $run = !!(count($arr) == $limit);
+
+                $leads = array_merge($leads, $arr);
+
+            } else {
+                $run = false;
+            }
+
+        }
+
+        $all_price = 0;
+        $cost_price = 0;
+        $paid = 0;
+        foreach ($leads as $lead) {
+            $all_price = $all_price + $lead->price;
+
+            if (isset($lead->custom_fields_values)) {
+                foreach ($lead->custom_fields_values as $field) {
+                    
+                    // Себестоимость
+                    if ($field->field_id == 75197) {
+                        $cost_price = $cost_price + $field->values[0]->value;
+                    }
+
+                    // Оплачено клиентом
+                    if ($field->field_id == 506505) {
+                        $paid = $paid + $field->values[0]->value;
+                    }
+                }
+            }
+        }
+
+        return [
+            'all_price' => $all_price,
+            'cost_price' => $cost_price,
+            'paid' => $paid,
+            'count' => count($leads)
+        ];
+    }
+
+    public function CountLeads(Request $request) {
+
+
+        $created_from = $request->input('created_from');
+        $created_to = $request->input('created_to');
+
+        $closed_from = $request->input('closed_from');
+        $closed_to = $request->input('closed_to');
+
+        $status = explode(',', $request->input('status'));
+
+        $statuses = [];
+
+        foreach ($status as $item) {
+            $statuses[] = [
+                'pipeline_id' => 2291194,
+                'status_id' => $item
+            ];
+        }
 
         $created_at = [
             'from' => strtotime($created_from),
@@ -324,7 +511,28 @@ class AmoCRMAnalyticsController extends Controller
         ];
     }
 
+    /**
+     * TODO
+     * Данные по фильтру
+     */
     public function Leads(Request $request) {
+
+        
+        // $res = $this->api->execute("/api/v4/leads/20323447");
+
+        $id = 20323447;
+
+        $status_id = 31519303;
+        
+        $after = "&filter[value_after][leads_statuses][0][pipeline_id]=2291194&filter[value_after][leads_statuses][0][status_id]=$status_id";
+
+        $f = "?filter[entity]=lead&filter[entity_id]=$id&filter[type]=lead_status_changed$after";
+
+        $res = $this->api->execute("/api/v4/events$f");
+
+
+        dd($res);
+
         $created_from = $request->input('created_from');
         $created_to = $request->input('created_to');
 
@@ -386,14 +594,6 @@ class AmoCRMAnalyticsController extends Controller
 
 
         dd($data);
-
-
-
-
-
-
-
-
 
 
 
