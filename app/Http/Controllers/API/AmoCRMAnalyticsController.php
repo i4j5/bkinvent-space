d731de49abd8ed830fa53b5dcfb753ebca71f5f9
Aleksandr Sazhin <i4j5@ya.ru>
2021-06-10 15:03:49 +0300

    -

--- app/Http/Controllers/API/AmoCRMAnalyticsController.php
+++ app/Http/Controllers/API/AmoCRMAnalyticsController.php
@@ -36,7 +36,7 @@ class AmoCRMAnalyticsController extends Controller
             
             $data = [
                 'filter' => [
-                    'pipeline_id' => [2291194],
+                    'pipeline_id' => [2291194, 4255462],
                     'created_at' => $created_at,
                     'page' => $page,
                 ],
@@ -439,86 +439,6 @@ class AmoCRMAnalyticsController extends Controller
         ];
     }
 
-    public function CountLeads(Request $request) {
-
-
-        $created_from = $request->input('created_from');
-        $created_to = $request->input('created_to');
-
-        $closed_from = $request->input('closed_from');
-        $closed_to = $request->input('closed_to');
-
-        $status = explode(',', $request->input('status'));
-
-        $statuses = [];
-
-        foreach ($status as $item) {
-            $statuses[] = [
-                'pipeline_id' => 2291194,
-                'status_id' => $item
-            ];
-        }
-
-        $created_at = [
-            'from' => strtotime($created_from),
-            'to' => strtotime("$created_to 23:59:59.000"),
-        ];
-
-        $closed_at = [
-            'from' => strtotime($closed_from),
-            'to' => strtotime("$closed_to 23:59:59.000"),
-        ];
-
-        $leads = [];
-        $limit = 200;
-
-        for ($page=1, $run=true; $run; $page++) { 
-            
-            $data = [
-                'filter' => [
-                    
-                    'statuses' => [
-                        0 => [
-                            'pipeline_id' => 2291194,
-                            'status_id' => 142
-                        ]
-                    ],
-                    'created_at' => $created_at,
-                    'closed_at' => $closed_at,
-                    'page' => $page,
-                ],
-                'limit' => $limit 
-            ];
-    
-            $query = http_build_query($data);
-    
-            $res = $this->api->execute("/api/v4/leads?$query");
-
-            if ($res) {
-
-                $arr = $res->_embedded->leads;
-
-                $run = !!(count($arr) == $limit);
-
-                $leads = array_merge($leads, $arr);
-
-            } else {
-                $run = false;
-            }
-
-        }
-
-        $all_price = 0;
-        foreach ($leads as $lead) {
-            $all_price = $all_price + $lead->price;
-        }
-
-        return [
-            'all_price' => $all_price, 
-            'count' => count($leads)
-        ];
-    }
-
     /**
      * TODO
      * Данные по фильтру
@@ -666,17 +586,22 @@ class AmoCRMAnalyticsController extends Controller
 
     private function RemoveTrash($leads) {
 
+        $loss_reason_ids = [
+            4074595, // Дубль
+            4104691, // Спам
+            5250691, // Не лид
+        ];
+
         foreach ($leads as $id => $lead) {
-            // 4074595// Дубль
-            // 4104691// Спам
             if (isset($lead->loss_reason_id)) {
-                if ($lead->loss_reason_id == 4104691 or $lead->loss_reason_id == 4074595) {
+
+                if ( in_array($lead->loss_reason_id, $loss_reason_ids) ) {
                     unset($leads[$id]);
                 }
+
             }
         }
 
         return $leads;
-
     }
 }
